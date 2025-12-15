6c8c68a7e7063169fc75805103c6adcf6f8c1058
ShanzidaDE <shanzidashanu1015@gmail.com>
2025-12-15 00:49:18 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+//Navigation
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('nav-links');
+
+hamburger.addEventListener('click', () => {
+  navLinks.classList.toggle('show');
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const currentPath = window.location.pathname.split("/").pop(); 
+  const navLinks = document.querySelectorAll(".nav-links a");
+  navLinks.forEach(link => {
+    link.classList.remove("active");
+    if (link.getAttribute("href") === currentPath) {
+      link.classList.add("active");
+    }
+  });
+});
+
+
+// Pop Up
+ window.addEventListener('DOMContentLoaded', () => {
+      const overlay = document.getElementById('welcomeOverlay');
+      const welcomeBtn = document.getElementById('welcomeBtn');
+
+      welcomeBtn.addEventListener('click', () => {
+        overlay.classList.add('hidden');
+      });
+    });
